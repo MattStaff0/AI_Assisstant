a2d53b63972f5a8f7d6396c60f6800034dd5dcb6
Matthew Stafford <mstafford135@gmail.com>
2026-02-03 21:29:06 -0600

    update model in main.py

--- src/main.py
+++ src/main.py
@@ -7,12 +7,15 @@ from gemini import GeminiClient
 # BCM numbering (GPIO21). Physical pin 40.
 btn = Button(21, pull_up=True, bounce_time=0.05)
 
-gemini = GeminiClient(model="gemini-2.0-flash")
+gemini = GeminiClient(model="gemini-2.5-flash-lite")
 
 def on_press():
     print(f"PRESSED  @ {time.strftime('%H:%M:%S')}")
+    # This is where the logic should be placed in order to record a voice, this is SPEECH -> TEXT
+    # After this we can either send the api response here or maybe in the on realease since this could just be updating a global variable and use it in on release!
 
 def on_release():
+    # This is where logic for turning the text to speech and then being able to output to speakers to hear
     print(f"RELEASED @ {time.strftime('%H:%M:%S')}")
     prompt = "Give me a one-sentence helpful tip for staying productive tonight."
     try:
